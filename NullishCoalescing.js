4decfc696936b6a3ad2a530c194766e16b7abb4c
Atul Sharma <atul.sharma@fcc-fac.ca>
2024-06-30 13:03:33 -0400

    day1

---
+++ NullishCoalescing.js
@@ -0,0 +1,8 @@
+///value1?? value2
+//it will return value2 if value1 is null or undefined
+
+let empName = null ?? 'Raja';
+let deptName = undefined ?? 'Contractor';
+
+console.log(empName);
+console.log(deptName);
