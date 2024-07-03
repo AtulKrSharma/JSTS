6000b29e82cee940248fd0ac428f7f6efacd66cd
Atul Sharma <atul.sharma@kyndryl.com>
2024-07-03 15:11:11 -0400

    day=3

---
+++ class.js
@@ -0,0 +1,40 @@
+class DummyClass {
+    constructor(name, year) {
+        this.name = name
+        this.year = year;
+    }
+
+    dummyName(numbers) {
+        let numbersIncrement = numbers + 1;
+        return numbersIncrement;
+    }
+
+    get name() {
+        return this.name
+    }
+
+    set name(name) {
+
+        if (name !== null && name !== undefined) {
+            this.name = name
+
+        }
+        else {
+            console.log('name is blank');
+
+        }
+
+
+    }
+}
+
+
+
+let dummyObj = new DummyClass('Atul', 1983);
+console.log(dummyObj);
+
+let values = dummyObj.dummyName(44)
+console.log(values);
+
+console.log(dummyObj.setName())
+
